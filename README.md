01cc36bd1cae3656884711b6fba24b251f8ff3ce
Felix Bytow <felix.bytow@autinity.de>
2016-08-15 10:07:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Task::BeLike::DRAKO
+
+This is a collection of perl packages I use in my everyday work.
+
+## Copyright & License
+
+Copyright © 2016, Felix Bytow <felix.bytow@googlemail.com>
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
---
+++ lib/Task/BeLike/DRAKO.pm
@@ -0,0 +1,92 @@
+package Task::BeLike::DRAKO;
+
+use 5.024;
+use utf8;
+use warnings;
+use strict;
+
+# ABSTRACT: be more like DRAKO -- use what he uses
+
+1;
+
+__END__
+
+=pod
+
+=pkgroup Archiving / Compression
+
+=pkg Archive::Zip For working with .zip archives
+
+=pkg Compress::LZ4Frame 0.011001 For fast compression and decompression of data
+
+
+
+=pkgroup Code Quality
+
+=pkg Perl::Critic The standard
+
+=pkg Perl::Critic::Lax Some not so strict rules for Perl::Critic
+
+=pkg Perl::Tidy The other standard
+
+=pkg Devel::Cover Check code coverage
+
+=pkg Const::Fast For named constants
+
+=pkg Try::Tiny::Retry
+
+This one is based on Try::Tiny, so it comes with try, catch and finally.
+It also comes with retry and some more functions allowing to try multiple
+times before failing.
+
+
+
+=pkgroup Performance
+
+=pkg Devel::NYTProf Great profiler for finding bottlenecks
+
+=pkg Inline::C For quick testing XS algorithms
+
+
+
+=pkgroup Web-Development
+
+=pkg Task::Dancer2 Dancer2 and useful addons
+
+
+
+=pkgroup Package management
+
+=pkg App::cpanminus My favorite cpan client
+
+=pkg App::cpm Maybe my new favorite cpan client
+
+
+
+=pkgroup Tools for building CPAN distributions
+
+=pkg Dist::Zilla 5 You don't want to make dists without it
+
+=pkg Dist::Zilla::PluginBundle::Starter Better than the old Basic
+
+=pkg Dist::Zilla::PluginBundle::Git Because everybody should use Git
+
+=pkg Dist::Zilla::Plugin::GithubMeta Useful for projects hosted on Github
+
+=pkg PodWeaver 4 For POD generation
+
+=pkg Pod::Elemental::Transformer::List So PodWeaver can make list cool
+
+
+
+=pkgroup Other libraries
+
+=pkg Moose The standard OO library
+
+=pkg Data::Dumper For all your variable dumping needs
+
+=pkg Log::Log4perl For logging
+
+=pkg Devel::PPPort For a better XS development experience
+
+=cut
